e1c62b73861f0a35c74d05d1c54d09a4825179df
Evgeniy Strelbitskiy <evgenstb@gmail.com>
2023-06-05 22:26:04 +0300

    add athorization and login routing

---
+++ routes/api/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ctrl = require("../../controllers/auth")
+
+const router = express.Router();
+const { validateBody } = require("../../middlewares");
+const {schemas} = require("../../models/user");
+
+
+
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+module.exports = router;
+
